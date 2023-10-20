089bb6e7c72e0e5765dec4fa1047ce539399ea41
yakovivan <yakovivan@mail.ru>
2023-10-20 17:45:24 +0300

    first commit

---
+++ src/main/java/com/ivanyakovlev/spring/springboot/springboot_rest/entity/Employee.java
@@ -0,0 +1,43 @@
+package com.ivanyakovlev.spring.springboot.springboot_rest.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "employees")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "surname")
+    private String surname;
+
+    @Column(name = "department")
+    private String department;
+
+    @Column(name = "salary")
+    private int salary;
+
+    public Employee(String name, String surname, String department, int salary) {
+
+        this.name = name;
+        this.surname = surname;
+        this.department = department;
+        this.salary = salary;
+    }
+}
